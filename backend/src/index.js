520028598a0a3fd50194facf207f225c9de1a7f6
Suprith R.Y <109782151+Suprith37@users.noreply.github.com>
2026-02-14 21:08:08 +0530

    Update CORS configuration in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,13 @@ import authRoute from "./models/routes/authRoute.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+  origin: ['https://event-horizon-jpezb6n30-suprith37s-projects.vercel.app'],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
